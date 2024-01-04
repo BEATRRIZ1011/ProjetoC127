5bdec30ce7f085162e16c92a986262b59deb2ed3
beatriz1011 <127145257+BEATRRIZ1011@users.noreply.github.com>
2024-01-04 15:14:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log(quickDatataset [randowNumber]);
+
+sketch = quickDrawDataset [randowNumber];
+
+timerCounter = 0;
+
+timerCheck = [];
+
+drawSketch = [];
+
+answerHolder = [];
+
+score = 0;
